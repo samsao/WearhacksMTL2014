5d81d65e57026897351562c1bbcad0b786b4a24e
Adrien CARANTA <adrien.caranta@gmail.com>
2014-09-27 18:26:35 -0400

    Separated storyboard for ScheduleVC

--- Wearhacks/StoryboardConstants.h
+++ Wearhacks/StoryboardConstants.h
@@ -5,6 +5,5 @@
 /* Controller identifier constants */
 extern NSString * const PROFILE_NAV_CONTROLLER;
 extern NSString * const PROFILE_VIEW_CONTROLLER;
-extern NSString * const REHAB_VIEW_CONTROLLER;
 extern NSString * const SCHEDULE_NAV_CONTROLLER;
 extern NSString * const SCHEDULE_VIEW_CONTROLLER;
